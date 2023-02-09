622a1c11ee31921b6f959244286644c40ec966c1
DRISS BENADJAL <driss.benadjal@edu.univ-eiffel.fr>
2023-02-09 13:59:41 +0100

    fix components path

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
-import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
+import NavBar from '../components/navbar'
+import Footer from '../components/footer'
 
 export default function App({ Component, pageProps }) {
   return (
