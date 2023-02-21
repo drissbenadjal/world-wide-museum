c937fc2ce4a9424561e2ee2e404c5de67b8c917b
drissbenadjal <driss.benadjal@hotmail.com>
2023-02-21 17:04:07 +0100

    add transition page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,17 @@
 import '@/styles/globals.css'
 import NavBar from '../components/navbar/navbar'
 import Footer from '../components/footer'
+import { SwitchTransition, CSSTransition } from 'react-transition-group'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <NavBar />
-      <Component {...pageProps} />
+      <SwitchTransition mode="out-in">
+        <CSSTransition key={Component} classNames="fade" timeout={400}>
+          <Component {...pageProps} />
+        </CSSTransition>
+      </SwitchTransition>
       <Footer />
     </>
   )
