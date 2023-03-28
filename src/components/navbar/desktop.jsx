90bc6b473c74383efcb708f041251ad328ee1b35
Théo Grassien <theo.grassien@gmail.com>
2023-03-28 14:23:18 +0200

    add favicon

--- src/components/navbar/desktop.jsx
+++ src/components/navbar/desktop.jsx
@@ -1,49 +1,69 @@
-import Button from "../button"
-import Link from 'next/link'
+import Button from "../button";
+import Link from "next/link";
 import Image from "next/image";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 //image
-import logo from '../../images/logo.svg'
-import logoBlue from '../../images/logo_blue.svg'
+import logo from "../../images/logo.svg";
+import logoBlue from "../../images/logo_blue.svg";
 
 export const Desktopnavbar = () => {
+  const router = useRouter();
 
-    const router = useRouter();
+  useEffect(() => {
+    //verifier quand on scroll si on est sur la page
+    document.addEventListener("scroll", () => {
+      if (window.scrollY > 20) {
+        document.querySelector("header").classList.add("nav-scroll");
+      } else {
+        document.querySelector("header").classList.remove("nav-scroll");
+      }
+    });
+  }, []);
 
-    useEffect(() => {
-        //verifier quand on scroll si on est sur la page
-        document.addEventListener('scroll', () => {
-            if (window.scrollY > 20) {
-                document.querySelector('header').classList.add('nav-scroll');
-            } else {
-                document.querySelector('header').classList.remove('nav-scroll');
-            }
-        });
-    }, []);
-
-    return (
-        <>
-            <header>
-                <Link href="/">
-                    <Image src={logo} alt="accueil" width={44} height={44} />
-                </Link>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link href="/decouvrir" className={router.pathname == "/decouvrir" ? "nav-active link" : "link"}>Découvrir</Link>
-                        </li>
-                        <li>
-                            <Link href="/collection" className={router.pathname == "/collection" ? "nav-active link" : "link"}>Collection</Link>
-                        </li>
-                        <li>
-                            <Button linkBtn='/billeterie'>Billeterie</Button>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </>
-    )
-
-}
+  return (
+    <>
+      <header>
+        <Link href="/">
+          <Image src={logo} alt="accueil" width={44} height={44} />
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link
+                href="/decouvrir"
+                className={
+                  router.pathname == "/decouvrir" ? "nav-active link" : "link"
+                }
+              >
+                Découvrir
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/collection"
+                className={
+                  router.pathname == "/collection" ? "nav-active link" : "link"
+                }
+              >
+                Collection
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://expo-interactive.vercel.app/"
+                className={"link"}
+              >
+                Expo Interactive
+              </Link>
+            </li>
+            <li>
+              <Button linkBtn="/billeterie">Billeterie</Button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
